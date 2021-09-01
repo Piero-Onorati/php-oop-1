b9071bb3b9fce389f70fd1f1b11533f59029c50f
Piero <piero.onorati@virgilio.it>
2021-09-01 15:44:14 +0200

    class creation + construct method

--- index1.php
+++ index1.php
@@ -1,2 +1,23 @@
 <?php
 
+//creazione classe
+class Movie{
+
+    // variabili/attributi
+    public $title;
+    public $director;
+    public $runtime;
+    public $IMDb_rating;
+    public $box_office;
+
+    //metodo costruttore
+    function __construct($_title, $_director){
+        $this->title = $_title;
+        $this->director = $_director;
+    }
+}
+
+//creazione istanze della classe
+$movie1 = new Movie('Back to Future', 'Robert Zemeckis');
+
+var_dump($movie1);
